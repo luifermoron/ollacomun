47c13ae649da9b36f9a008326730ac7737dcf0fc
mypathofficial <mypathofficial@gmail.com>
2019-11-07 16:07:52 -0400

    fixing url errors  and making migrations

--- places/urls.py
+++ places/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url,include
-from django.contrib import admin
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^api/v1/places/', include('places.urls')),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
