d51af515560592b557294fa52fa1929d2a8f261f
mypathofficial <mypathofficial@gmail.com>
2019-11-08 01:50:24 -0400

    update requirements, and add postgress support

--- ollacomun/settings.py
+++ ollacomun/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ollacomun.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ollacomun',
+        'USER': 'postgres',
+        'PASSWORD': 'luiferdb',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
