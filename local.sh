46c4fdbcfdfef87fc253d56f1c02ffcc48f67b4a
Luis Fernando Morón Dorado <luifer.2341@gmail.com>
2021-11-10 16:33:00 -0400

    modify local.sh

--- local.sh
+++ local.sh
@@ -1,3 +1,6 @@
+echo "### Pulling changes ###"
+git pull origin master
+
 echo "### Stop old version ###"
 docker-compose down
 rm -rf /root/apps/backend/static
@@ -11,7 +14,7 @@ docker-compose run backend python manage.py migrate
 echo "### Collect Static ###"
 docker-compose run backend python manage.py collectstatic
 
-echo "### Move Statics ###"
-rm -rf /var/www/static
-cp -R /root/apps/backend/static /var/www/static
-chgrp -R www-data /var/www/static
+#echo "### Move Statics ###"
+##rm -rf /var/www/static
+#cp -R /root/apps/backend/static /var/www/static
+#chgrp -R www-data /var/www/static
