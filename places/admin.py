51ed6eaf593e544b7f3d72c4839416de5a2b0a2e
Luis Fernando Morón Dorado <luifer.2341@gmail.com>
2021-11-16 08:36:49 -0400

    add images

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 # the module name is app_name.models
 
 # Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
-from places.models import Place
+from places.models import Image, Place
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
 
 class PlaceAdmin(admin.ModelAdmin):
     exclude = ('is_active', 'uuid')
+    inlines = [ImageInline, ]
 
 admin.site.register(Place, PlaceAdmin)
